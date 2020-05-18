c22869cec20c066cf0552e25438fc88dd54341f9
Nick Butler <nwmbutler@gmail.com>
2020-05-18 16:32:44 +0100

    Add the balance

---
+++ lib/oystercard.rb
@@ -0,0 +1,13 @@
+class Oystercard
+
+  def initialize
+    @balance = 0
+  end
+
+  def balance
+    @balance
+  end
+
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
+
+
+  it "has a balance" do
+  expect(Oystercard.new.balance).not_to be nil
+  end
+
 end
