413f1f6abc67fd7bd3443cbde5eaa589c8916e04
Student <student@Makerss-MacBook-Air.local>
2020-05-18 16:39:49 +0100

    Add the balance B

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,9 @@
 class Oystercard
 
+  attr_reader :balance
+
   def initialize
     @balance = 0
   end
-
-  def balance
-    @balance
-  end
-
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,6 @@ require 'oystercard'
 
 describe Oystercard do
 
-
   it "has a balance" do
   expect(Oystercard.new.balance).not_to be nil
   end
