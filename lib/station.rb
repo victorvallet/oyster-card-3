d1776a77e5cfff0cdff9bdae8e2f11aab88a287f
minsi yang <yangminsi@gmail.com>
2020-05-21 14:20:18 +0100

    Revert back to step 13

--- lib/station.rb
+++ lib/station.rb
@@ -1,12 +1,5 @@
 
 class Station
   
-  def initialize(zone)
-    @zone = zone
-  end
-
-  def zone
-    @zone
-  end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,4 @@
 require 'station'
 describe Station do
 
-  it 'initialize a station with zone value' do
-    station = Station.new(num)
-    expect(station.zone).to eq num
-  end
-
-  it 'show the zone of the station' do
-    station = Station.new(1)
-    expect(station.zone).to eq 1
-  end
 end
