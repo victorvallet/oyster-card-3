23ba400b267d987c4bdfaea0e1ef77d1e5b188bf
victorvallet <victorvallet8@gmail.com>
2020-05-21 16:06:09 +0200

    added zone number to station.rb and station_spec.rb

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,7 +2,11 @@ require 'station'
 describe Station do
 
   it "return the name of the station" do
-    station = Station.new("Cricklewood")
+    station = Station.new("Cricklewood", 1)
     expect(station.name).to eq "Cricklewood"
+  end
+  it 'returns the zone number of the station' do
+    station = Station.new("bank", 1)
+    expect(station.zone).to eq 1
   end
 end
